c858aaca31c4b2f128707c61cf7278e3fe39e0ba
MintedKitten <87274176+MintedKitten@users.noreply.github.com>
2022-11-19 11:01:01 +0700

    Workshop A

---
+++ workShopA.js
@@ -0,0 +1,23 @@
+const user = {
+  name: "Korndanai",
+  nickname: "Sky",
+  hobby: "Listening to Music",
+  salary: 35000,
+  address: "My home",
+  province: {
+    province: "Bangkok",
+    postcode: "10310",
+  },
+};
+
+const showUserData = (user) => {
+  const {
+    nickname,
+    hobby,
+    province: { postcode },
+  } = user;
+  return `My nickname is ${nickname}, My hobby is ${hobby}
+and my postcode is ${postcode}`;
+};
+
+console.log(showUserData(user));
